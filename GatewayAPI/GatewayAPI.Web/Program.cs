1503543d5299979101ff907ffa6549b47abfd22c
Ivan Andreevets <ivan_grozdny@mail.ru>
2023-06-14 16:45:34 +0300

    Add ocelot configuration

--- GatewayAPI/GatewayAPI.Web/Program.cs
+++ GatewayAPI/GatewayAPI.Web/Program.cs
@@ -1,6 +1,20 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
 
 app.Run();
