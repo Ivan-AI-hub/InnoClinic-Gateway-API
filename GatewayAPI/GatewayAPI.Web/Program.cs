4f654448cd15ea5a8dfbba3025f815c36a694058
Ivan Andreevets <ivan_grozdny@mail.ru>
2023-06-16 11:55:57 +0300

    Add docker support

--- GatewayAPI/GatewayAPI.Web/Program.cs
+++ GatewayAPI/GatewayAPI.Web/Program.cs
@@ -1,4 +1,5 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,5 +17,7 @@ app.UseSwaggerForOcelotUI(opt =>
 {
     opt.PathToSwaggerGenerator = "/swagger/docs";
 });
+app.UseRouting();
+app.UseOcelot();
 
 app.Run();
